0a23d7bbc5ce9f06d514cfbf60597845f3f162a8
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-07-24 14:41:30 +0900

    update

--- basnet/configuration_basnet.py
+++ basnet/configuration_basnet.py
@@ -6,13 +6,10 @@ class BASNetConfig(PretrainedConfig):
 
     def __init__(
         self,
-        resnet_model: str = "microsoft/resnet-34",
         n_channels: int = 3,
         kernel_size: int = 3,
         **kwargs,
     ) -> None:
         super().__init__(**kwargs)
-        self.resnet_model = resnet_model
         self.n_channels = n_channels
-
-        self.kernel_size = 3
+        self.kernel_size = kernel_size
